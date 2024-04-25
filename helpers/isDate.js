b9b37d2d59df36400125d185c826c493baeadbd7
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2024-04-25 13:12:53 -0400

    Mejora en la validacion de hora

--- helpers/isDate.js
+++ helpers/isDate.js
@@ -1,12 +1,33 @@
-const { isValid } = require('date-fns');
+const moment = require('moment');
+
+const isDate = ( value ) => {
+
+    if ( !value ) {
+        return false;
+    }
+
+    const fecha = moment( value );
+    if ( fecha.isValid() ) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+
+
+module.exports = { isDate };
+
+// const { isValid } = require('date-fns');
      
-const isDate = (dateValue) => {
-  if (!dateValue) return false;
+// const isDate = (dateValue) => {
+//   if (!dateValue) return false;
  
-  const date = isValid(dateValue); // retorna true o false
-  return date;
-};
+//   const date = isValid(dateValue); // retorna true o false
+//   return date;
+// };
  
-module.exports = {
-  isDate
-} 
+// module.exports = {
+//   isDate
+// } 
