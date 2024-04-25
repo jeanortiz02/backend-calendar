8c19b20ad7f958c6f8a43e80de586eb68c12e5b4
Jean Carlos Ortiz <jeancarlosortizperez02@gmail.com>
2024-04-25 15:13:35 -0400

    correccion de rutas

--- index.js
+++ index.js
@@ -26,10 +26,11 @@ app.use( express.json() );
 // Rutas
 app.use( '/api/auth', require('./routes/auth') );
 app.use( '/api/events', require('./routes/events') );
-//TODO: CRUD: Eventos
 
-
-
+// Cualquier ruta
+app.get( '*', (req, res) => {
+    res.sendFile( __dirname + '/public/index.html')
+})
 
 // Escuchar peticiones
 app.listen( process.env.PORT , () => {
